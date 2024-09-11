6e96601d11ff620a91db5065c9a01df6fa202f7f
hari-yahoo <115019601+hari-yahoo@users.noreply.github.com>
2024-09-11 18:31:38 +0530

    .

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 
 # Define the Blueprint for the main module
-main = Blueprint('main', __name__, template_folder='templates')
+main_bp = Blueprint('main_bp', __name__, template_folder='templates')
 
 # Import the routes
 from . import routes
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,10 +1,13 @@
 from flask import render_template
-from . import main
+from . import main_bp as main
+from flask_login import login_required
 
 @main.route('/')
 def index():
     return render_template('index.html')
 
+
 @main.route('/about')
+@login_required
 def about():
     return render_template('about.html')
